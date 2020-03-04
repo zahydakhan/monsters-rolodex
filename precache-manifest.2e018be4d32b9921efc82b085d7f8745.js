ed120b272e7015a8172a725d6379bca9a32b1ffd
Zahida <zahydakhan@gmail.com>
2020-03-04 08:57:51 +0500

    Updates

---
+++ precache-manifest.2e018be4d32b9921efc82b085d7f8745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7b27179811fc57019ed221ac08fb3a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "feb79e6e4d81145a373f",
+    "url": "/monsters-rolodex/static/css/main.93ffd83f.chunk.css"
+  },
+  {
+    "revision": "5c57c03053ce039126c3",
+    "url": "/monsters-rolodex/static/js/2.8cbda136.chunk.js"
+  },
+  {
+    "revision": "779022913f67bd8202aa70fa2261f4c5",
+    "url": "/monsters-rolodex/static/js/2.8cbda136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb79e6e4d81145a373f",
+    "url": "/monsters-rolodex/static/js/main.6e43bc6f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
